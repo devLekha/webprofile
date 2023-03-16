de38abcd2f3668b774ec87bc87121954ae60db85
devLekha <lekhasaraf09@gmail.com>
2023-03-16 15:14:30 +0530

    favicon and title updated

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -5,14 +5,18 @@ import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import {Box} from '@mui/material'
 const Contact = ()=>{
-    return(<Box>
+    return(<Box style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItem:'center'}}>
         <h1>Contact</h1>
+       <Box><SmartphoneIcon/>91+ 9424624464</Box> 
+        <Box><EmailIcon/>lekhasaraf09@gmail.com</Box>
+        <a href="https://www.linkedin.com/in/lekha-saraf-524907187" target="_blank" rel="noreferrer">
         <LinkedInIcon/>
-        <SmartphoneIcon/>
-        <EmailIcon/>
+        </a>
         <h3>Work Refrence</h3>
-        <GitHubIcon/>
         <a href="https://stackoverflow.com/users/10380319/crypto-s?tab=profile" target="_blank" rel="noreferrer">stackoverflow</a>
+        <a href="https://github.com/devLekha" target="_blank" rel="noreferrer">
+        <GitHubIcon/>
+        </a>
         </Box>
     )
 }
